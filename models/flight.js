6710a323a926ae710ebcd4209a3a1fbe0b051b1e
andrewwinke123 <andrewwinke@gmail.com>
2023-04-16 00:47:52 -0400

    adding my flight model and setting up its schema

--- models/flight.js
+++ models/flight.js
@@ -5,6 +5,16 @@ const Schema = mongoose.Schema
 const flightSchema = new Schema( {
   airline: {type: String, enum: ['American', 'Southwest', 'United']},
   airport: {type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'], default: 'DEN'},
-  flightNo: {type: Number, required: 10-9999},
-  departs: {type: Date, default: }
+  flightNo: {type: Number, required: true, min: 10, max: 9999},
+  departs: {type: Date, default: function() {
+    return new Date.now()}
+  },
+}, {
+  timestamps: true
 })
+
+const Flight = mongoose.model('Flight', movieSchema)
+
+export {
+  Movie,
+}
